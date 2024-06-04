bdde189d2db8eb7e8f46b269210834642712b786
efekanyuksel4 <efekanyuksel4@hotmail.com>
2024-06-04 11:39:49 +0100

    I am having with problem merghe into github so this is a new branch I am going to try in here so there is no changing

--- AdminSystem/OrderManagementSystemConfirmDelete.aspx.cs
+++ AdminSystem/OrderManagementSystemConfirmDelete.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClassLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,8 +8,22 @@ using System.Web.UI.WebControls;
 
 public partial class _1_ConfirmDelete : System.Web.UI.Page
 {
+    Int32 OrderId;
     protected void Page_Load(object sender, EventArgs e)
     {
+        OrderId = Convert.ToInt32(Session["Order_ID"]);
+    }
 
+    protected void Yesbtn_Click(object sender, EventArgs e)
+    {
+        clsOrderCollection AllOrders = new clsOrderCollection();
+        AllOrders.ThisOrder.Find(OrderId);
+        AllOrders.Delete();
+        Response.Redirect("OrderManagementSystemList.aspx");
+    }
+
+    protected void Nobtn_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("OrderManagementSystemList.aspx");
     }
 }
